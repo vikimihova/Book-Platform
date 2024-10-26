f25dd7b80fcb1413dfe6f0de3d62abdee5cbdc5f
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-10-26 19:45:07 +0300

    Create models
    - create Quote model
    - create Review model

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Data.Models/Quote.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookPlatform.Data.Models
+{
+    public class Quote
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        public Guid BookId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+    }
+}
---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Data.Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookPlatform.Data.Models
+{
+    public class Review
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        public Guid BookId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+    }
+}
