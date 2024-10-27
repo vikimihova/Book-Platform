8533af5b8450891f7d3ac8d7490fa88ab9a8e0bf
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-10-27 22:07:05 +0200

    feat(models): add enumeration for reading status

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Data.Models/ReadingStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPlatform.Data.Models
+{
+    public enum ReadingStatus
+    {
+        Read = 1,
+        CurrentlyReading = 2,
+        WantToRead = 3,
+    }
+}
