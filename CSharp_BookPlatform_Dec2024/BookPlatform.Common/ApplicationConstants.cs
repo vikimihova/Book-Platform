f3d4a53f2c4ce77becb5c6f991beaa2727c63da6
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-01 22:58:11 +0200

    build(common): add entity validation and application constants

--- CSharp_BookPlatform_Dec2024/BookPlatform.Common/ApplicationConstants.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Common/ApplicationConstants.cs
@@ -2,6 +2,7 @@
 {
     public static class ApplicationConstants
     {
-
+        public const string NoImageUrl = "/images/no-image.png";
+        public const string DateViewFormat = "dd MMMM yyyy";
     }
 }
