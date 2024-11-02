e9f106ed41104e6ea576cb2e6128a2e28e994e25
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-02 18:25:28 +0200

    feat: add index page for browsing books
    Added a book controller and an index page with a corresponding view model.

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Book/BookIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookPlatform.Web.ViewModels.Book
+{
+    public class BookIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+        
+        public string Title { get; set; } = null!;
+                
+        public string Author { get; set; } = null!;
+
+        public string Genre { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+        
+        public double AverageRating { get; set; }
+    }
+}
