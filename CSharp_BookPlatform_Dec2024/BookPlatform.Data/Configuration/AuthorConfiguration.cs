af8b81bd67090d39ab405fcbc8f9c3288aa70e6c
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-02 23:09:19 +0200

    build(data): add entity type configuration for Author and seed authors to the database

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Data/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using BookPlatform.Data.Models;
+using BookPlatform.Services.Data.DataProcessor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookPlatform.Data.Configuration
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasData(Seeder.GenerateAuthors());
+        }               
+    }
+}
