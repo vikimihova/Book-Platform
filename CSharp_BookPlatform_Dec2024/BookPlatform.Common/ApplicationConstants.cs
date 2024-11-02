44b5f6d4483abeb8254b1b06ae0eee4809abd75c
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-02 23:07:39 +0200

    build(services): add data processor in service layer
    Added data processor sublayer for deserialization from JSON files and generation of entity instances for seeding the database.
    - class Deserializer
    - class Seeder
    - Datasets folder for storage of JSON files
    - string application constant for folder path

--- CSharp_BookPlatform_Dec2024/BookPlatform.Common/ApplicationConstants.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Common/ApplicationConstants.cs
@@ -1,8 +1,11 @@
-﻿namespace BookPlatform.Common
+﻿using System.Globalization;
+
+namespace BookPlatform.Common
 {
     public static class ApplicationConstants
     {
         public const string NoImageUrl = "/images/no-image.png";
         public const string DateViewFormat = "dd MMMM yyyy";
+        public const string DataSetsPath = "../../../BookPlatform.Services.Data/DataProcessor/Datasets";
     }
 }
---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/DataProcessor/ImportDtos/BookImportDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookPlatform.Common.EntityValidationConstants.AuthorValidationConstants;
+using static BookPlatform.Common.EntityValidationConstants.BookValidationConstants;
+
+namespace BookPlatform.Services.Data.DataProcessor.ImportDtos
+{
+    public class BookImportDto
+    {
+        [Required]
+        [MinLength(AuthorFirstNameMinLength + AuthorLastNameMinLength)]
+        [MaxLength(AuthorFirstNameMaxLength + AuthorLastNameMaxLength)]
+        public string Author { get; set; } = null!;
+                
+        public string? Country { get; set; }
+
+        [Required]
+        [MaxLength(MaxImageUrlLength/2)]
+        public string ImageLink { get; set; } = null!;
+
+        public string? Language { get; set; }
+
+        [Required]
+        [MaxLength(MaxImageUrlLength / 2)]
+        public string Link { get; set; } = null!;
+        
+        public int? Pages { get; set; }
+
+        [Required]
+        [MinLength(TitleMinLength)]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public int Year { get; set; }
+    }
+}
