0114db649cb2f202e59f2daf5718f2568e8345dc
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-12 23:28:53 +0200

    feat: add a book details view

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Book/BookDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookPlatform.Web.ViewModels.Book
+{
+    public class BookDetailsViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string Genre { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+    }
+}
