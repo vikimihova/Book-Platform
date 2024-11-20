fdd9dbc2f6f348d0d650c150398932d9d7b1f073
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-20 23:22:26 +0200

    feat: add functionality for adding book to reading list

--- CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/ReadingListService.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/ReadingListService.cs
@@ -2,6 +2,7 @@
 using BookPlatform.Data.Repository.Interfaces;
 using BookPlatform.Services.Data.Interfaces;
 using BookPlatform.Web.ViewModels.ReadingList;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookPlatform.Services.Data
 {
@@ -23,17 +24,54 @@ namespace BookPlatform.Services.Data
             throw new NotImplementedException();
         }
 
-        public Task<bool> AddBookToUserReadingListAsync(string? bookId, string userId, int readingStatusId)
+        public async Task<bool> AddBookToUserReadingListAsync(string? bookId, string userId, int readingStatusId)
         {
             // check Guid bookId (if false, return false)
+            Guid bookGuid = Guid.Empty;
+
+            if (!this.IsGuidValid(bookId, ref bookGuid))
+            {
+                return false;
+            }
+
             // check if book exists (if null, return false)
+            Book? book = await this.bookRepository
+                .GetByIdAsync(bookGuid);
+
+            if (book == null)
+            {
+                return false;
+            }
+
+            // parse userId to Guid
+            Guid userGuid = Guid.Parse(userId);
+
             // check bookApplicationUser exists
+            BookApplicationUser? bookApplicationUser = await this.bookApplicationUserRepository
+                .GetAllAttached()
+                .FirstOrDefaultAsync(bau => bau.BookId == bookGuid && bau.ApplicationUserId == userGuid);
+
             // if false, create new bookApplicationUser
             // set reading status
-            // add to dbSet and save Changes
-            // return true
+            if (bookApplicationUser == null)
+            {
+                BookApplicationUser newBookApplicationUserToAdd = new BookApplicationUser()
+                {
+                    BookId = bookGuid,
+                    ApplicationUserId = userGuid,
+                    DateAdded = DateTime.Now,
+                    ReadingStatusId = readingStatusId
+                };
 
-            throw new NotImplementedException();
+                // add to dbSet and save Changes
+                await this.bookApplicationUserRepository.AddAsync(newBookApplicationUserToAdd);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
         }
 
         public Task<bool> RemoveBookFromUserReadingListAsync(string? bookId, string userId)
--- CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/ReadingListController.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/ReadingListController.cs
@@ -18,17 +18,34 @@ namespace BookPlatform.Web.Controllers
             this.userManager = userManager;
         }
 
-        public async Task<IActionResult> AddToReadingList(string bookId, string readingStatusId)
+        public async Task<IActionResult> AddToReadingList(string bookId, int readingStatusId)
         {
             // get UserId
+            string userId = this.userManager.GetUserId(this.User)!;
 
             // check UserId StringNullOrEmpty (if false, redirect)
+            if (String.IsNullOrWhiteSpace(userId))
+            {
+                return RedirectToPage("/Identity/Account/Login");
+            }
 
             // invoke method from ReadingListService
+            bool result = await this.readingListService
+                .AddBookToUserReadingListAsync(bookId, userId, readingStatusId);
 
             // if false, redirect
+            if (result == false)
+            {
+                TempData["ErrorMessage"] = "Book already in reading list.";
+                return RedirectToAction("Details", "Book", new { id = bookId });
+            }
+            else
+            {
+                // send temp data for alert message
+                TempData["SuccessMessage"] = "Book successfully added to your reading list!";
+            }            
 
-            return RedirectToAction("Details", "Book");
+            return RedirectToAction("Details", "Book", new { id = bookId });
         }
     }
 }
