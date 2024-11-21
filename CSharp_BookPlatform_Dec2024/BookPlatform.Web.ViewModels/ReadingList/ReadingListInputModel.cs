037a53bca60b907236f9c7a35d846d095e1e088b
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-21 19:00:03 +0200

    feat(reading-list): create reading list input model when adding a book to list

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/ReadingList/ReadingListInputModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static BookPlatform.Common.EntityValidationConstants.BookValidationConstants;
+using static BookPlatform.Common.EntityValidationConstants.ReviewValidationConstants;
+using static BookPlatform.Common.EntityValidationConstants.CharacterValidationConstants;
+
+namespace BookPlatform.Web.ViewModels.ReadingList
+{
+    public class ReadingListInputModel
+    {
+        [Required]
+        public string BookId { get; set; } = null!;
+
+        [Required]
+        [MinLength(TitleMinLength)]
+        [MaxLength(TitleMaxLength)]
+        public string BookTitle { get; set; } = null!;
+
+        public int? Rating { get; set; }
+
+        [Required]
+        public int ReadingStatus { get; set; }
+
+        public string? DateStarted { get; set; }
+
+        public string? DateFinished { get; set; }
+
+        [Required]
+        public string DateAdded { get; set; } = null!;
+
+        [MinLength(CharacterNameMinLength)]
+        [MaxLength(CharacterNameMaxLength)]
+        public string? FavoriteCharacter { get; set; }
+
+        [MinLength(ReviewContentMinLength)]
+        [MaxLength(ReviewContentMaxLength)]
+        public string? Review { get; set; }
+    }
+}
