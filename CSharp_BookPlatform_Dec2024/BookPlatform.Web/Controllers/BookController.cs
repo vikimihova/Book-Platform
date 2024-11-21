65aaa8f9a3f73144b7177935c9a4c0b1704c7cc3
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-21 18:25:43 +0200

    build(controllers): set TempData in book controller using reading list service

--- CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/BookController.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/BookController.cs
@@ -1,5 +1,7 @@
-﻿using BookPlatform.Services.Data.Interfaces;
+﻿using BookPlatform.Data.Models;
+using BookPlatform.Services.Data.Interfaces;
 using BookPlatform.Web.ViewModels.Book;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookPlatform.Web.Controllers
@@ -7,10 +9,17 @@ namespace BookPlatform.Web.Controllers
     public class BookController : Controller
     {
         private readonly IBookService bookService;
+        private readonly IReadingListService readingListService;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public BookController(IBookService _bookService)
+        public BookController(
+            IBookService _bookService,
+            IReadingListService readingListService,
+            UserManager<ApplicationUser> userManager)
         {
-            this.bookService = _bookService;            
+            this.bookService = _bookService;
+            this.readingListService = readingListService;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -24,7 +33,28 @@ namespace BookPlatform.Web.Controllers
 
         public async Task<IActionResult> Details(string bookId)
         {
-            BookDetailsViewModel model = await this.bookService.GetBookDetailsAsync(id);
+            // set TempData for reading status
+            if (this.User?.Identity?.IsAuthenticated ?? false)
+            {
+                ReadingStatus? readingStatus = null;
+
+                ApplicationUser? currentUser = await userManager.FindByNameAsync(this.User.Identity.Name!);
+
+                if (currentUser != null)
+                {
+                    // invoke method from ReadingListService
+
+                    readingStatus = await this.readingListService.GetReadingStatusForCurrentBookApplicationUserAsync(bookId, currentUser.Id);
+                }
+
+                if (readingStatus != null)
+                {
+                    TempData["ReadingStatus"] = readingStatus.StatusDescription;
+                } 
+            }
+
+            // generate view model
+            BookDetailsViewModel? model = await this.bookService.GetBookDetailsAsync(bookId);
 
             if (model == null)
             {
