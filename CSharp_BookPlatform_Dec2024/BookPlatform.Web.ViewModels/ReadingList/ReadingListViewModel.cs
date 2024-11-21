856e2f9a8e4cb6430f2384b5102b7af0f7faf348
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-21 18:59:43 +0200

    feat(reading-list): create reading list view model

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/ReadingList/ReadingListViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BookPlatform.Web.ViewModels.ReadingList
+{
+    public class ReadingListViewModel
+    {
+        public string BookId { get; set; } = null!;
+
+        public string BookTitle { get; set; } = null!;
+
+        public int? Rating { get; set; }
+
+        public string ReadingStatus { get; set; } = null!;
+
+        public string? DateStarted { get; set; }
+
+        public string? DateFinished { get; set; }
+
+        public string DateAdded { get; set; } = null!;
+
+        public string? FavoriteCharacter { get; set; }
+
+        public string? Review { get; set; }
+    }
+}
