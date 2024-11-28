4b838bf91ff1e4e17631579f518ac78c124bd7e8
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-28 19:13:17 +0200

    build: add friend controller and discover controller

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/DiscoverController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookPlatform.Web.Controllers
+{
+    public class DiscoverController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/FriendController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookPlatform.Web.Controllers
+{
+    public class FriendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
