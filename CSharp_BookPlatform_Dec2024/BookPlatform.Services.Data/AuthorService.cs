cabc254d9c68ed2dd091905bc7138ad86bfc3633
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-28 18:34:56 +0200

    build: add author service

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/AuthorService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using BookPlatform.Core.Services.Interfaces;
+using BookPlatform.Core.ViewModels.Author;
+
+using BookPlatform.Data.Models;
+using BookPlatform.Data.Repository.Interfaces;
+
+namespace BookPlatform.Core.Services
+{
+    public class AuthorService : BaseService, IAuthorService
+    {
+        private readonly IRepository<Author, Guid> authorRepository;
+
+        public AuthorService(IRepository<Author, Guid> authorRepository)
+        {
+            this.authorRepository = authorRepository;
+        }
+
+        public async Task<ICollection<SelectAuthorViewModel>> GetAuthorsAsync()
+        {
+            ICollection<SelectAuthorViewModel> authors = await this.authorRepository
+                .GetAllAttached()
+                .Where(a => a.IsDeleted == false)
+                .Select(a => new SelectAuthorViewModel()
+                {
+                    Id = a.Id.ToString(),
+                    FullName = a.FullName
+                })
+                .ToListAsync();
+
+            return authors;
+        }
+    }
+}
