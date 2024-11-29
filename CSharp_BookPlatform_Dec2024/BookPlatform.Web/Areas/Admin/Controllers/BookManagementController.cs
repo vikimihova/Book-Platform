23ca64a53aff84110e13b7d2edfd2207c1e077b4
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-30 00:28:56 +0200

    build: add controllers to admin area

---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Areas/Admin/Controllers/BookManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookPlatform.Web.Areas.Admin.Controllers
+{
+    public class BookManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,69 @@
+﻿using BookPlatform.Core.Services.Interfaces;
+using BookPlatform.Core.ViewModels.Admin.UserManagement;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static BookPlatform.Common.ApplicationConstants;
+
+namespace BookPlatform.Web.Areas.Admin.Controllers
+{
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class UserManagementController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserManagementController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<AllApplicationUsersViewModel> model = await this.userService
+                .GetAllUsersAsync();
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MakeAdmin(string userId)
+        {
+            bool result = await this.userService.MakeAdminAsync(userId);
+
+            if (!result)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveAdmin(string userId)
+        {
+            bool result = await this.userService.RemoveAdminAsync(userId);
+
+            if (!result)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            bool result = await this.userService.DeleteUserAsync(userId);
+
+            if (!result)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
