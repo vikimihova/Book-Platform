b55ee6bc718810b34bd392821dcb17bd7eaf9093
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-12-04 22:03:29 +0200

    fix: add exception type to custom exception middleware

--- CSharp_BookPlatform_Dec2024/BookPlatform.Web.Infrastructure/Middleware/CustomExceptionMiddleware.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.Infrastructure/Middleware/CustomExceptionMiddleware.cs
@@ -26,21 +26,16 @@ namespace BookPlatform.Web.Infrastructure.Middleware
 
         private static Task HandleExceptionAsync(HttpContext context, Exception exception) 
         { 
-            context.Response.ContentType = "text/html"; 
+            context.Response.ContentType = "text/html";                        
 
-            //if (exception is NullReferenceException) 
-            //{ 
-            //    context.Response.StatusCode = (int)HttpStatusCode.BadRequest; 
-            //    return context.Response.WriteAsync("<html><body><h1>400 Bad Request</h1><p>Invalid input provided.</p></body></html>"); 
-            //}
-
-            if (exception is ArgumentNullException)
+            if (exception is ArgumentException ||
+                exception is ArgumentNullException)
             {
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                 return context.Response.WriteAsync("<html><body><h1>400 Bad Request</h1><p>Invalid input provided.</p></body></html>");
             }
 
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             return context.Response.WriteAsync("<html><body><h1>500 Internal Server Error</h1><p>An unexpected error occurred.</p></body></html>");
         }
     }        
