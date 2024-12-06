f86fbdc88abc4a59f585a3b566c11be3c6b5094f
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-12-06 23:51:43 +0200

    feat: add favourite character to review view model

--- CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Review/ReviewViewModel.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Review/ReviewViewModel.cs
@@ -6,10 +6,16 @@
 
         public string BookId { get; set; } = null!;
 
+        public string Title { get; set; } = null!;
+
         public string Author { get; set; } = null!;
 
         public string Content { get; set; } = null!;
 
         public bool IsModified { get; set; } = false;
+
+        public int? Rating { get; set; }
+
+        public string? FavoriteCharacter { get; set; }
     }
 }
