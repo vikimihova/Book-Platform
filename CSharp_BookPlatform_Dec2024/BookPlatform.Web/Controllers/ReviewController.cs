1caa1a3122becccfa1854b6899d4efb7d8a37069
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-12-08 16:56:57 +0200

    fix: catch exceptions

--- CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/ReviewController.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web/Controllers/ReviewController.cs
@@ -5,6 +5,7 @@ using BookPlatform.Core.Services.Interfaces;
 using BookPlatform.Core.ViewModels.Review;
 
 using BookPlatform.Web.Infrastructure.Extensions;
+using BookPlatform.Core.ViewModels.ReadingList;
 
 namespace BookPlatform.Web.Controllers
 {
@@ -23,10 +24,19 @@ namespace BookPlatform.Web.Controllers
         public async Task<IActionResult> Index()
         {
             // get user id
-            string userId = User.GetUserId()!;            
+            string userId = User.GetUserId()!;
 
             // generate view model
-            IEnumerable<ReviewViewModel> model = await this.reviewService.GetAllNewReviewsAsync(userId);
+            IEnumerable<ReviewViewModel> model;      
+            
+            try
+            {
+                model = await this.reviewService.GetAllNewReviewsAsync(userId);
+            }
+            catch (Exception ex) when (ex is ArgumentException || ex is InvalidOperationException)
+            {
+                return BadRequest();
+            }         
 
             return View(model);
         }
@@ -35,12 +45,17 @@ namespace BookPlatform.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> AllBookReviews(string bookId)
         {
-            if (string.IsNullOrWhiteSpace(bookId))
+            // generate view model
+            IEnumerable<ReviewViewModel> model;
+
+            try
+            {
+                model = await this.reviewService.GetAllReviewsPerBookAsync(bookId);
+            }
+            catch (Exception ex) when (ex is ArgumentException || ex is InvalidOperationException)
             {
                 return BadRequest();
             }
-
-            IEnumerable<ReviewViewModel> model = await this.reviewService.GetAllReviewsPerBookAsync(bookId);
 
             return View(model);
         }        
