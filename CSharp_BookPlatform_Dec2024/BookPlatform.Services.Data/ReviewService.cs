488a3ee984677e86d651ea175db6213e4d9d402a
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-12-08 16:56:43 +0200

    fix: set tracking behaviour and perform checks

--- CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/ReviewService.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Services.Data/ReviewService.cs
@@ -1,55 +1,64 @@
 ﻿using BookPlatform.Core.Services.Interfaces;
-using BookPlatform.Core.ViewModels.ApplicationUser;
 using BookPlatform.Core.ViewModels.Review;
 using BookPlatform.Data.Models;
 using BookPlatform.Data.Repository.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
-using System.Runtime.CompilerServices;
 
 namespace BookPlatform.Core.Services
 {
     public class ReviewService : BaseService, IReviewService
     {
         private readonly IRepository<Review, Guid> reviewRepository;
+        private readonly IRepository<Book, Guid> bookRepository;
         private readonly UserManager<ApplicationUser> userManager;
 
         public ReviewService(
             IRepository<Review, Guid> reviewRepository,
+            IRepository<Book, Guid> bookRepository,
             UserManager<ApplicationUser> userManager)
         {
             this.reviewRepository = reviewRepository;
+            this.bookRepository = bookRepository;
             this.userManager = userManager;
         }
 
         public async Task<IEnumerable<ReviewViewModel>> GetAllNewReviewsAsync(string userId)
         {
-            IEnumerable<ReviewViewModel> reviews = new List<ReviewViewModel>();
-
-            // check if user id is a valid guid
+            // check if userId is a valid guid
             Guid userGuid = Guid.Empty;
             if (!IsGuidValid(userId, ref userGuid))
             {
-                return reviews;
-            }
+                throw new ArgumentException();
+            }                   
 
             // find user
             ApplicationUser? user = await this.userManager
                 .Users
+                .AsNoTracking()
                 .Include(u => u.UserBooks)
                 .FirstOrDefaultAsync(u => u.Id == userGuid);
 
-            if (user == null || user.LastLogin == null)
+            if (user == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // generate empty model
+            IEnumerable<ReviewViewModel> reviews = new List<ReviewViewModel>();
+
+            if (user.LastLogin == null)
             {
                 return reviews;
             }
 
+            // get bookIds for UserBooks
             var userBookIds = user.UserBooks.Select(ub => ub.BookId).ToList();
 
-            // generate reviews
+            // generate review view models
             reviews = await this.reviewRepository
-                .GetAllAttached()                
+                .GetAllAttached()        
+                .AsNoTracking()
                 .Include(r => r.BookApplicationUser)
                 .ThenInclude(bau => bau.Character)
                 .Include(r => r.BookApplicationUser)
@@ -78,15 +87,32 @@ namespace BookPlatform.Core.Services
 
         public async Task<IEnumerable<ReviewViewModel>> GetAllReviewsPerBookAsync(string bookId)
         {
+            // check if bookId is a valid guid
+            Guid bookGuid = Guid.Empty;
+            if (!IsGuidValid(bookId, ref bookGuid))
+            {
+                throw new ArgumentException();
+            }
+
+            // find book
+            Book? book = await this.bookRepository.GetByIdAsync(bookGuid);
+
+            if (book == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // generate review view models
             IEnumerable<ReviewViewModel> reviews = await this.reviewRepository
                 .GetAllAttached()
+                .AsNoTracking()
                 .Include(r => r.BookApplicationUser)
                 .ThenInclude(bau => bau.Character)
                 .Include(r => r.BookApplicationUser)
                 .ThenInclude(bau => bau.Book)
                 .Include(r => r.BookApplicationUser)
                 .ThenInclude(bau => bau.ApplicationUser)
-                .Where(r => r.BookId.ToString().ToLower() == bookId.ToLower())
+                .Where(r => r.BookId == bookGuid)
                 .Select(r => new ReviewViewModel()
                 {
                     Id = r.Id.ToString(),
