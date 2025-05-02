050d4b648d16890831f83eb4fb8706f959143ba6
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-05-02 16:15:51 +0300

    view-models: expand QuoteViewModel

--- CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Quote/QuoteViewModel.cs
+++ CSharp_BookPlatform_Dec2024/BookPlatform.Web.ViewModels/Quote/QuoteViewModel.cs
@@ -2,6 +2,10 @@
 {
     public class QuoteViewModel
     {
+        public string Title { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
         public string Content { get; set; } = null!;
     }
 }
